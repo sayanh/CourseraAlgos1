a24ee9523f903b9c980c405d79b28bae8a41edd8
sayanh <sayan.mnit@gmail.com>
2016-01-29 07:52:17 +0100

    Initial commit

---
+++ src/main/java/algos/QuickFindUnion.java
@@ -0,0 +1,31 @@
+package algos;
+
+/**
+ * Created by shazra on 1/29/16.
+ */
+public class QuickFindUnion {
+
+    private int[] id;
+
+    public QuickFindUnion(int N) {
+        id = new int[N];
+        for ( int i = 0; i < N; i++ ) {
+            id[i] = i;
+        }
+    }
+
+
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        for ( int i = 0; i < id.length; i++ ) {
+            if (id[i] == pid) {
+                id[p] = qid;
+            }
+        }
+    }
+}
---
+++ src/test/java/algos/QuickFindUnionTest.java
@@ -0,0 +1,14 @@
+package algos;
+
+import org.junit.Test;
+
+/**
+ * Created by shazra on 1/29/16.
+ */
+public class QuickFindUnionTest {
+
+    @Test
+    public void basicQFTest() {
+        QuickFindUnion quickFindUnion = new QuickFindUnion();
+    }
+}
